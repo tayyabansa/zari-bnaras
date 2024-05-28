8db7783f6c81c0efae3787877a5edd48c69afa6f
unknown <mhtayyabdev@gmail.com>
2024-05-28 14:21:55 +0530

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\ParallelTesting;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Schema::defaultStringLength(191);
+
+        ParallelTesting::setUpTestDatabase(function (string $database, int $token) {
+            Artisan::call('db:seed');
+        });
+        Schema::defaultStringLength(190);
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+
+    }
+}
